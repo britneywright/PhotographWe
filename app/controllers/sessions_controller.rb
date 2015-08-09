fb113b8ef73b50a58f1288396de07145560f9874
britneywright <blw06g@gmail.com>
2015-08-09 01:48:25 -0400

    fix session controller typos

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
     if user.try(:authenticate, params[:username])
       session[:user_id] = user.id
-      redirect_to user, notice "Welcome back!"
+      redirect_to user, notice: "Welcome back!"
     else
       flash[:errors] = ["Invalid login"]
       redirect_to root_path
@@ -13,6 +13,6 @@ class SessionsController < ApplicationController
 
   def logout
     session.clear
-    redirect_to_root_path, notice: "Logged out successfully"
+    redirect_to root_path, notice: "Logged out successfully"
   end
 end
