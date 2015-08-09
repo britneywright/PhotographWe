7477fa64f7d6382a487ca58c5bd3f496e50d7626
britneywright <blw06g@gmail.com>
2015-08-09 01:35:46 -0400

    add login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
   resources :users
+  get 'signup' => 'users#new'
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#logout'
 end
