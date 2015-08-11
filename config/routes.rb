3df8e0272e559ffb8fd451fcb3af8e0ddb3fc267
Britney Wright <blw06g@gmail.com>
2015-08-11 08:40:04 -0400

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_pages#index'
 
   resources :users
   resources :albums
+  
   get 'signup' => 'users#new'
-
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   get 'logout' => 'sessions#logout'
