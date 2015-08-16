38acc2d4ccf2619e9e52970d5c16c1ebd1a0ee64
britneywright <blw06g@gmail.com>
2015-08-16 18:36:59 -0400

    add photograph routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   resources :users do
     resources :albums
   end
+
+  resources :albums do
+    resources :photographs
+  end
+
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
