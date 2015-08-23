3b455e1cc8fcff136d9be0c3a82ab50f8c1b9d51
Britney Wright <blw06g@gmail.com>
2015-08-22 22:37:22 -0400

    upload and add photos to album

--- app/controllers/photographs_controller.rb
+++ app/controllers/photographs_controller.rb
@@ -4,6 +4,39 @@ class PhotographsController < ApplicationController
     @photograph = Photograph.find_by(id: params[:id])
   end
 
+  def new
+    @album = Album.find_by(id: params[:album_id])
+    @photograph = Photograph.new
+  end
+
+  def create
+    album = Album.find_by(id: params[:album_id])
+    photograph = album.photographs.new(photograph_params)
+    if current_user
+      photograph.uploader = current_user
+    end
+    if photograph.save
+      redirect_to album_photograph_path(album.id, photograph.id)
+    else
+      flash[:errors] = photograph.errors.full_messages
+      redirect_to album
+    end
+  end
+
+  def upload
+    @album = Album.find_by(id: params[:album_id])
+  end
+
+  def add
+    @album = Album.find_by(id: params[:album_id])
+    if params[:photographs]
+      params[:photographs].each do |source|
+        @album.photographs.create(source: source)
+      end
+    end
+    redirect_to @album
+  end
+
   private
 
   def photograph_params
