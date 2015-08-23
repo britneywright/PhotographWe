f6a188a1c0c046d3e4961251afcbc98689e1c453
Britney Wright <blw06g@gmail.com>
2015-08-22 22:44:53 -0400

    fix album and photograph associations

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,7 @@
 class Album < ActiveRecord::Base
   belongs_to :user
-  has_many :photographs
+  has_many :photographs, :dependent => :destroy
+  accepts_nested_attributes_for :photographs
   validates :name, :user, presence: true
   with_options if: :private_album? do |album|
     album.validates :secret_key, presence: true
--- app/models/photograph.rb
+++ app/models/photograph.rb
@@ -1,7 +1,9 @@
 class Photograph < ActiveRecord::Base
   belongs_to :album
   belongs_to :uploader, class_name: 'User'
-  has_attached_file :source, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :source, 
+    :styles => { :medium => "300x300#", :thumb => "200x200#" }, 
+    :default_url => "/images/:style/missing.png"
   validates :album, presence: true
   validates_attachment_content_type :source, :content_type => /\Aimage\/.*\Z/
   validates :source, :attachment_presence => true
