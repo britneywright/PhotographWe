3648cfe5c3d04b873570d1afe478f4ae91458600
Britney Wright <blw06g@gmail.com>
2015-08-24 01:32:08 -0400

    set slug before validation

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,9 +2,25 @@ class Album < ActiveRecord::Base
   belongs_to :user
   has_many :photographs, :dependent => :destroy
   accepts_nested_attributes_for :photographs
-  validates :name, :user, presence: true
+  validates :name, :user, :slug, presence: true
   with_options if: :private_album? do |album|
     album.validates :secret_key, presence: true
     album.validates :secret_key, length: {minimum: 6}
+  end
+
+  before_validation :set_slug
+ 
+  def to_param
+    slug
+  end 
+  
+  protected
+
+  def set_slug
+    if slug.nil? || slug.empty?
+      self.slug = name.parameterize
+    else
+      self.slug = self.slug.parameterize
+    end
   end
 end
